b5b3a9f1c2bff3d2db2529f4dd424ff511bcc469
Boubkir <109249622+Boubkir@users.noreply.github.com>
2023-06-17 03:12:53 +0200

    added new components and routerlinks

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AddTaskComponent } from './add-task/add-task.component';
+import { SummaryComponent } from './summary/summary.component';
+import { BoardComponent } from './board/board.component';
+import { ContactsComponent } from './contacts/contacts.component';
+import { LegalNoticeComponent } from './legal-notice/legal-notice.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
 
 
 const routes: Routes = [
+  { path: 'summary', component: SummaryComponent },
+  { path: 'board', component: BoardComponent },
   { path: 'add-task', component: AddTaskComponent },
+  { path: 'contacts', component: ContactsComponent },
+  { path: 'legal-notice', component: LegalNoticeComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent },
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,12 @@ import { AppComponent } from './app.component';
 import { AddTaskComponent } from './add-task/add-task.component';
 import { AsideComponent } from './aside/aside.component';
 import { HeaderComponent } from './header/header.component';
+import { SummaryComponent } from './summary/summary.component';
+import { BoardComponent } from './board/board.component';
+import { ContactsComponent } from './contacts/contacts.component';
+import { LegalNoticeComponent } from './legal-notice/legal-notice.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
 
 @NgModule({
   declarations: [
@@ -12,6 +18,12 @@ import { HeaderComponent } from './header/header.component';
     AddTaskComponent,
     AsideComponent,
     HeaderComponent,
+    SummaryComponent,
+    BoardComponent,
+    ContactsComponent,
+    LegalNoticeComponent,
+    LoginComponent,
+    RegisterComponent,
   ],
   imports: [
     BrowserModule,
