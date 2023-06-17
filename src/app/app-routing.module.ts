5e7aa363c77d694d066acef84320329168f7c654
Boubkir <109249622+Boubkir@users.noreply.github.com>
2023-06-17 02:57:24 +0200

    added aside and header

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AddTaskComponent } from './add-task/add-task.component';
 
-const routes: Routes = [{ 
-  path: '', component: AddTaskComponent }];
+
+const routes: Routes = [
+  { path: 'add-task', component: AddTaskComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
 export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AddTaskComponent } from './add-task/add-task.component';
@@ -12,7 +11,7 @@ import { HeaderComponent } from './header/header.component';
     AppComponent,
     AddTaskComponent,
     AsideComponent,
-    HeaderComponent
+    HeaderComponent,
   ],
   imports: [
     BrowserModule,
